af2fc1870a14965fcba146cacb84d3e22cc990b1
Tedko <teodorstanishev@gmail.com>
2018-03-16 15:44:17 +0200

    MapEditor added

---
+++ src/MapEditor/MapEditor.h
@@ -0,0 +1,39 @@
+// #include <VGA.h>
+// #include <MapEditor/Sprites.h>
+#include <Bitmap/GameObject.h>
+// #include <Controls/Keyboard.h>
+
+#pragma once
+
+#ifndef MAP_EDITOR_H
+#define MAP_EDITOR_H
+
+
+#define WIDTH 320
+#define HEIGHT 240
+
+#define TRANSPARENT -256
+
+
+class MapEditor{
+  public:
+    int xCursor,x_prev;
+    int yCursor,y_prev;
+    int BACKGROUND_COLOR=0;
+    MapEditor();
+    void begin();
+    void update(int *x,int *y);
+    void moveCursor();
+    void selectPosition(int *x,int *y);
+
+    void setBackgroundColor(int color);//TODO
+    int getBackgroundColor();
+    void getSprites();
+    void drawSpriteSelector();
+    void drawMap_1d(unsigned int _map[]);
+    void drawMap_2d(unsigned int _map[][20]);
+
+  private:
+};
+
+#endif
